6ba08721287a6d7117a64d4459332619f40d6cd8
Sharifov Aslbek <148238968+sharifov-aslbek@users.noreply.github.com>
2024-06-24 13:09:09 +0500

    added google maps

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,15 @@
 <template>
 <HeroSection />
 <AboutSection />
+<LocationSection />
 </template>
 
 <script>
 import HeroSection from '@/components/home/HeroSection.vue'
-import AboutSection from '@/components/home/AboutSection.vue';
+import AboutSection from '@/components/home/AboutSection.vue'
+import LocationSection from '@/components/home/LocationSection.vue'
 export default {
-   components: {HeroSection , AboutSection}
+   components: {HeroSection , AboutSection , LocationSection}
 }
 </script>
 
