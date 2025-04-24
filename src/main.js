da85e0d60833c5b07069022bd50c1811568c3482
sharifov-aslbek <sharifovaslbek5@gmail.com>
2025-04-24 19:27:35 +0500

    Fixed error

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import i18n from './i18n.js'
+import { createHead } from '@vueuse/head'
 import component from './components/ui-components/component.js'
 import {MotionPlugin} from '@vueuse/motion'
 const app = createApp(App)
+const head = createHead()
+app.use(head)
 component.map(component => app.component(component.name , component))   
 app.use(i18n)
 app.use(router)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,7 @@
    <LocationSection />
  </template>
  
- <script>
+ <script setup>
  import { useHead } from '@vueuse/head'
  import Modal from '@/components/home/Modal.vue'
  import HeroSection from '@/components/home/HeroSection.vue'
@@ -18,66 +18,53 @@
  import TimeSection from '@/components/home/TimeSection.vue'
  import Video from '@/components/home/Video.vue'
  
- export default {
-   components: {
-     HeroSection,
-     AboutSection,
-     LocationSection,
-     AdvantageSection,
-     TimeSection,
-     Video,
-     Modal
-   },
-   setup() {
-      useHead({
-  title: 'Kelajak School Qarshi – Xususiy Maktab',
-  meta: [
-    {
-      name: 'description',
-      content: 'Kelajak School Qarshi – zamonaviy va innovatsion taʼlim metodlariga asoslangan Qarshidagi xususiy maktab. Farzandingizning kelajagi biz bilan yorqin bo‘ladi!'
-    },
-    {
-      name: 'keywords',
-      content: 'Kelajak School, Qarshi xususiy maktab, taʼlim, maktab, zamonaviy maktab, innovatsion taʼlim, xususiy maktab Qarshi, boshlang‘ich maktab, o‘rta maktab, zamonaviy o‘qituvchilar, taʼlim texnologiyalari, maktabda STEM, ingliz tili, IT fanlari'
-    },
-    {
-      property: 'og:title',
-      content: 'Kelajak School Qarshi – Xususiy Maktab'
-    },
-    {
-      property: 'og:description',
-      content: 'Qarshidagi eng zamonaviy va innovatsion yondashuvga ega xususiy maktab. Kelajak avlodni tarbiyalaymiz.'
-    },
-    {
-      property: 'og:image',
-      content: 'https://kelajakschoolqarshi.uz/og-image.jpg' // Rasm bo‘lsa shu yerga joylashtiring
-    },
-    {
-      name: 'twitter:card',
-      content: 'summary_large_image'
-    },
-    {
-      name: 'twitter:title',
-      content: 'Kelajak School Qarshi – Xususiy Maktab'
-    },
-    {
-      name: 'twitter:description',
-      content: 'Innovatsion taʼlim, zamonaviy maktab va iqtidorli yoshlar uchun imkoniyat – Kelajak School Qarshi.'
-    },
-    {
-      name: 'twitter:image',
-      content: 'https://kelajakschoolqarshi.uz/og-image.jpg'
-    }
-  ],
-  link: [
-    {
-      rel: 'canonical',
-      href: 'https://kelajakschoolqarshi.uz/'
-    }
-  ]
-})
-   }
- }
+ useHead({
+   title: 'Kelajak School Qarshi – Xususiy Maktab',
+   meta: [
+     {
+       name: 'description',
+       content: 'Kelajak School Qarshi – zamonaviy va innovatsion taʼlim metodlariga asoslangan Qarshidagi xususiy maktab. Farzandingizning kelajagi biz bilan yorqin bo‘ladi!'
+     },
+     {
+       name: 'keywords',
+       content: 'Kelajak School, Qarshi xususiy maktab, taʼlim, maktab, zamonaviy maktab, innovatsion taʼlim, xususiy maktab Qarshi, boshlang‘ich maktab, o‘rta maktab, zamonaviy o‘qituvchilar, taʼlim texnologiyalari, maktabda STEM, ingliz tili, IT fanlari'
+     },
+     {
+       property: 'og:title',
+       content: 'Kelajak School Qarshi – Xususiy Maktab'
+     },
+     {
+       property: 'og:description',
+       content: 'Qarshidagi eng zamonaviy va innovatsion yondashuvga ega xususiy maktab. Kelajak avlodni tarbiyalaymiz.'
+     },
+     {
+       property: 'og:image',
+       content: 'https://kelajakschoolqarshi.uz/og-image.jpg'
+     },
+     {
+       name: 'twitter:card',
+       content: 'summary_large_image'
+     },
+     {
+       name: 'twitter:title',
+       content: 'Kelajak School Qarshi – Xususiy Maktab'
+     },
+     {
+       name: 'twitter:description',
+       content: 'Innovatsion taʼlim, zamonaviy maktab va iqtidorli yoshlar uchun imkoniyat – Kelajak School Qarshi.'
+     },
+     {
+       name: 'twitter:image',
+       content: 'https://kelajakschoolqarshi.uz/og-image.jpg'
+     }
+   ],
+   link: [
+     {
+       rel: 'canonical',
+       href: 'https://kelajakschoolqarshi.uz/'
+     }
+   ]
+ })
  </script>
  
  <style>
